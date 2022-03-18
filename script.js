371a8faac31920cb7b8d8ae342ef1be8aa7e5145
bitama <niyibizipatrick1@gmail.com>
2022-03-18 13:48:32 -0500

    a

---
+++ script.js
@@ -0,0 +1,21 @@
+const videoElement = document.getElementById('video');
+        const button = document.getElementById('button');
+        
+        //Prompt to select media stream ,pass to video element and then play
+        async function selectMediaStream(){
+        try {
+            const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+            videoElement.srcObject=mediaStream;
+            videoElement.onloadedmetadata=()=>videoElement.play();
+        }
+        catch(error){
+            console.log('Whoops!!,error here',error)
+        }
+        }
+        selectMediaStream();
+
+        button.addEventListener('click',async ()=>{
+        button.disabled=true;
+        await videoElement.requestPictureInPicture()
+        button.disabled=false
+        }); 
